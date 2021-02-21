8b1b9be97b766a70a020544d6e2af41431411cc1
Mihai Blebea <mihai@getchip.uk>
2021-02-21 00:35:44 +0000

    Adds seed

--- lib/platform_release.ex
+++ lib/platform_release.ex
@@ -1,6 +1,8 @@
 defmodule PlatformRelease do
     @app :platform
 
+    @seed_file "seed.exs"
+
     alias Platform.Article
 
     def migrate do
@@ -16,11 +18,6 @@ defmodule PlatformRelease do
         {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
     end
 
-    def seed() do
-        load_app()
-        for article <- articles(), do: article |> Article.save
-    end
-
     defp repos do
         Application.fetch_env!(@app, :ecto_repos)
     end
@@ -29,9 +26,45 @@ defmodule PlatformRelease do
         Application.load(@app)
     end
 
-    defp articles() do
-        "seed/articles.json"
-        |> File.read!
-        |> Jason.decode!
+    @spec seed() :: list
+    def seed() do
+        load_app()
+
+        for repo <- repos() do
+            case Ecto.Migrator.with_repo(repo, &eval_seed(&1, @seed_file)) do
+                {:ok, {:ok, _fun_return}, _apps} ->
+                    :ok
+
+                {:ok, {:error, reason}, _apps} ->
+                    IO.warn(reason)
+                    {:error, reason}
+
+                {:error, term} ->
+                    IO.warn(term, [])
+                    {:error, term}
+            end
+        end
+    end
+
+    defp eval_seed(repo, filename) do
+        seeds_file = get_path(repo, "seeds", filename)
+
+        if File.regular?(seeds_file) do
+            {:ok, Code.eval_file(seeds_file)}
+        else
+            {:error, "Seeds file not found."}
+        end
+    end
+
+    defp get_path(repo, directory, filename) do
+        priv_dir = "#{:code.priv_dir(@app)}"
+
+        repo_underscore =
+            repo
+            |> Module.split()
+            |> List.last()
+            |> Macro.underscore()
+
+        Path.join([priv_dir, repo_underscore, directory, filename])
     end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Platform.{Article, User}
+
+Article.save(%{
+    title: "Build a concurrent, concurrent server that can handle millions of requests with Golang",
+    subtitle: "Just a subtitle",
+    slug: "golang-concurrent-multithreaded-server-with-cache",
+    image_url: "https://thetravelvertical.com/wp-content/uploads/2019/11/dog-3822183_1920-1024x608.jpg",
+    description: "This is the second article",
+    content_url: "https://raw.githubusercontent.com/MihaiBlebea/mihaiblebea-content/master/golang_concurrency_cache_website.md"
+})
+
+Article.save(%{
+    title: "Build a Go Quiz Game with concurrent jobs from scratch in 6 easy steps",
+    subtitle: "Just a subtitle",
+    slug: "go-concurrent-quiz-game",
+    image_url: "https://www.sbs.com.au/guide/sites/sbs.com.au.guide/files/styles/full/public/8-out-of-10-cats.jpg",
+    description: "Coding a game that involves concurrency must be sooooo complicated... right? Not really! It is quite easy by using Go routines system. Let me show you how to do it.",
+    content_url: "https://raw.githubusercontent.com/MihaiBlebea/mihaiblebea-content/master/go_concurrent_quiz_game.md"
+})
+
+User.save(%{
+    name: "Mihai",
+    email: "mihaiserban.blebea@gmail.com",
+    password: "intrex",
+    role_id: 1,
+    marketing_consent: true
+})
